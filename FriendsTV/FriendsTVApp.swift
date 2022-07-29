bc44c6852a17e7a26d196b39efd4c9595c44e58e
Andrew Buyanov <dantees1a@gmail.com>
2022-07-29 12:42:41 +0700

    Initial Commit

---
+++ FriendsTV/FriendsTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsTVApp.swift
+//  FriendsTV
+//
+//  Created by Андрей Буянов on 29.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct FriendsTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
